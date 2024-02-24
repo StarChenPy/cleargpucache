7b62faaa35385628251c62188dce2fde5afca380
StarChen <3425316708@qq.com>
2024-02-24 13:13:25 +0800

    Initial commit

---
+++ src/main/java/xyz/starchenpy/cleargpucache/ClearGpuCache.java
@@ -0,0 +1,12 @@
+package xyz.starchenpy.cleargpucache;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(ClearGpuCache.MODID)
+public class ClearGpuCache {
+    public static final String MODID = "cleargpucache";
+
+    public ClearGpuCache() {
+        // 这是一个纯 Mixin Mod, 所以这里甚至不需要写东西
+    }
+}
---
+++ src/main/java/xyz/starchenpy/cleargpucache/mixin/ClearGpuCacheMixin.java
@@ -0,0 +1,25 @@
+package xyz.starchenpy.cleargpucache.mixin;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.LevelRenderer;
+import org.lwjgl.opengl.GL11;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(LevelRenderer.class)
+public class ClearGpuCacheMixin {
+    @Final
+    @Shadow
+    private Minecraft minecraft;
+
+    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/LevelRenderer;compileChunks(Lnet/minecraft/client/Camera;)V", shift = At.Shift.AFTER, by = 1), method = "renderLevel")
+    public void renderLevelMixin(CallbackInfo ci){
+        this.minecraft.getProfiler().popPush("finish");
+        GL11.glFinish();
+        this.minecraft.getProfiler().popPush("terrain");
+    }
+}
